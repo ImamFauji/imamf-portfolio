9f295bebd955642d5277827a3c1164f55f53489e
Imam Fauji <imamgaul71@gmail.com>
2025-10-01 18:42:36 +0700

    space header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,6 +6,7 @@ import { motion } from "motion/react";
 const Header = () => {
   return (
     <header className="w-full bg-[url('/planes_cover.jpg')] bg-cover bg-fixed bg-center bg-no-repeat">
+        
       <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
         <motion.div
           initial={{ scale: 0.5 }}
